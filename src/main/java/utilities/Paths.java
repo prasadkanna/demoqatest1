07f0bf70ecb2309b3b557733fe809df12af93f5e
satya <prasadparamalla@gmail.com>
2023-09-14 18:11:56 +0530

    added all files

---
+++ src/main/java/utilities/Paths.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public interface Paths {
+
+	String config ="C:\\Users\\BOSU\\git\\demoqatest1\\src\\test\\resources\\properties\\Config.properties";
+	String excel ="C:\\Users\\BOSU\\git\\demoqatest1\\src\\main\\java\\testdata\\excel1.xlsx";
+}
